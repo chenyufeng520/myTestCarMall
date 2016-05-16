4a8f681cfe62dd5164d8b47b8fa8fbdc546abdd0
陈宇峰 <958222182@qq.com>
2016-05-16 23:06:53 +0800

    整理

--- CarLife/MainClasses/Modules/登录/Helper/LoginCenter.m
+++ CarLife/MainClasses/Modules/登录/Helper/LoginCenter.m
@@ -25,40 +25,6 @@
 + (void)doLogin:(NSDictionary *)info
 {
     ISTBaseViewController *vc = (ISTBaseViewController *)[info objectForKey:kLoginDelegate];
-////    ColumnType columnType = (ColumnType)[[info objectForKey:kColumnType] intValue];
-////    NSInteger menuIndex = [[info objectForKey:kMenuIndex] integerValue];
-//    AnimationType animationType = (AnimationType)[[info objectForKey:kAnimateType] intValue];
-////    UIType uitype = (UIType)[[info objectForKey:kUIType] intValue];
-//    ISTBaseViewController *loginVC = [[ISTBaseViewController alloc] initWithNibName:nil bundle:nil];
-    
-//    ISTLoginView *registerOrLoginView = nil;
-//    if(uitype != ForgetType){
-//        registerOrLoginView = [[ISTLoginView alloc] initWithFrame:CGRectMake(0, 0, kScreen_Width, kScreen_Height)];
-//        [registerOrLoginView showWithType:columnType];
-//    }
-//    else{
-//        registerOrLoginView = [[ISTLoginView alloc] initForgetModeFrame:CGRectMake(0, 0, kScreen_Width, kScreen_Height)];
-//    }
-//    registerOrLoginView.vc = loginVC;
-//    registerOrLoginView.animationType = animationType;
-//    registerOrLoginView.uitype = uitype;
-//    registerOrLoginView.menuIndex = menuIndex;
-   
-//    loginVC.view = loginV.view;
-//    
-//    //UITableView,UIScrollView的时候有时候会偏移20像素
-//    if ([[UIDevice currentDevice] systemVersion].floatValue>=7.0) {
-//        loginVC.automaticallyAdjustsScrollViewInsets = NO;
-//    }
-//    
-//    if(animationType == PresentType){
-//        ISTBaseNavigationController *theNavigation = [[ISTBaseNavigationController alloc] initWithRootViewController:loginVC];
-//        theNavigation.navigationBarHidden = YES;
-//        [vc presentViewController:theNavigation animated:YES completion:NULL];
-//    }
-//    else if(animationType == PushType)
-//    {
-//        [vc.navigationController pushViewController:loginVC animated:YES];
-//    }
+
 }
 @end
