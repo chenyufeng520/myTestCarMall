ea1c368bc6c27fc89bd67e607a6e6e88978d0a6c
聂康 <964972709@qq.com>
2016-06-12 09:02:18 +0800

    提交

--- CarLife/MainClasses/Modules/商城/Controllers/GoodsDetailViewController.m
+++ CarLife/MainClasses/Modules/商城/Controllers/GoodsDetailViewController.m
@@ -12,7 +12,9 @@
 @interface GoodsDetailViewController ()<UITextFieldDelegate,UITextViewDelegate>{
     UITextField *_numTextField;
     UITextView *_msgTextView;
-    CGFloat _previousTextViewContentHeight;
+    UILabel *_totalLab;
+    UIView *_bottomView;
+    CGFloat _keyBoardHeight;
 }
 @end
 
@@ -35,11 +37,13 @@
 {
     _tbTop = [self creatTopBarView:kTopFrame];
     [self.view addSubview:_tbTop];
+    _contentView.height += kTabBarHeight;
 }
 
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.automaticallyAdjustsScrollViewInsets = NO;
     [self addNotification];
     [self loadSubviews];
     [self initWithModel];
@@ -70,7 +74,7 @@
     [_contentView addSubview:introLab];
     
     //描述一下部分
-    UIView *firstLine = [[UIView alloc] initWithFrame:CGRectMake(10, introLab.maxY+5, kScreen_Width-10, 0.8)];
+    UIView *firstLine = [[UIView alloc] initWithFrame:CGRectMake(10, introLab.maxY+5, kScreen_Width-10, 1)];
     firstLine.backgroundColor = [UIColor lightGrayColor];
     [_contentView addSubview:firstLine];
     
@@ -95,6 +99,7 @@
     _numTextField.layer.borderWidth = 1;
     _numTextField.layer.borderColor = [UIColor grayColor].CGColor;
     _numTextField.text = @"1";
+    _numTextField.returnKeyType = UIReturnKeyDone;
     _numTextField.textAlignment = NSTextAlignmentCenter;
     _numTextField.delegate = self;
     [_contentView addSubview:_numTextField];
@@ -110,7 +115,7 @@
     [subtractBtn setTitleColor:[UIColor grayColor] forState:UIControlStateNormal];
     [_contentView addSubview:subtractBtn];
 
-    UIView *secondLine = [[UIView alloc] initWithFrame:CGRectMake(10, buyNumLab.maxY+1, kScreen_Width-10, 0.8)];
+    UIView *secondLine = [[UIView alloc] initWithFrame:CGRectMake(10, buyNumLab.maxY+1, kScreen_Width-10, 1)];
     secondLine.backgroundColor = [UIColor lightGrayColor];
     [_contentView addSubview:secondLine];
     
@@ -120,7 +125,7 @@
     [_contentView addSubview:stockLab];
     
     UILabel *stockRightLab = [[UILabel alloc] initWithFrame:CGRectMake(kScreen_Width-110, secondLine.maxY, 100, kAdjustLength(140))];
-    if ([self.productModel.goods_lock isEqualToString:@"1"]){
+    if (self.productModel.goods_lock.intValue == 1){
         stockRightLab.text = @"有货";
     }else{
         stockRightLab.text = @"已售空";
@@ -130,7 +135,7 @@
     stockRightLab.textAlignment = NSTextAlignmentRight;
     [_contentView addSubview:stockRightLab];
     
-    UIView *thirdLine = [[UIView alloc] initWithFrame:CGRectMake(10, stockLab.maxY, kScreen_Width-10, 0.8)];
+    UIView *thirdLine = [[UIView alloc] initWithFrame:CGRectMake(10, stockLab.maxY, kScreen_Width-10, 1)];
     thirdLine.backgroundColor = [UIColor lightGrayColor];
     [_contentView addSubview:thirdLine];
 
@@ -140,6 +145,14 @@
     userMsgLab.frame = CGRectMake(10, thirdLine.maxY+2, STRING_WIDTH(userMsgLab.text, kAdjustLength(140), kFont_16), kAdjustLength(140));
     [_contentView addSubview:userMsgLab];
     
+//    NSTextStorage* textStorage = [[NSTextStorage alloc] init];
+//    NSLayoutManager* layoutManager = [NSLayoutManager new];
+//    [textStorage addLayoutManager:layoutManager];
+//    NSTextContainer *textContainer = [[NSTextContainer alloc] initWithSize:CGSizeMake(kScreen_Width-10-userMsgLab.maxX-5-20, kAdjustLength(105)-20)];
+//    textContainer.widthTracksTextView = YES;
+//    textContainer.heightTracksTextView = YES;
+//    [layoutManager addTextContainer:textContainer];
+//    _msgTextView = [[UITextView alloc] initWithFrame:CGRectMake(userMsgLab.maxX+5, thirdLine.maxY+kAdjustLength(25), kScreen_Width-10-userMsgLab.maxX-5, kAdjustLength(105)) textContainer:textContainer];
     _msgTextView = [[UITextView alloc] initWithFrame:CGRectMake(userMsgLab.maxX+5, thirdLine.maxY+kAdjustLength(25), kScreen_Width-10-userMsgLab.maxX-5, kAdjustLength(105))];
     _msgTextView.font = kFontLarge_1;
     _msgTextView.autocorrectionType = UITextAutocorrectionTypeNo;
@@ -147,8 +160,49 @@
     _msgTextView.layer.borderColor = [UIColor lightGrayColor].CGColor;
     _msgTextView.layer.borderWidth = 0.8;
     _msgTextView.delegate = self;
+    _msgTextView.returnKeyType = UIReturnKeyDone;
+    _msgTextView.layoutManager.allowsNonContiguousLayout = NO;
     [_contentView addSubview:_msgTextView];
-    _previousTextViewContentHeight = [self _getTextViewContentH:_msgTextView];
+    
+    _bottomView = [[UIView alloc] initWithFrame:CGRectMake(0, _msgTextView.maxY+kAdjustLength(25), kScreen_Width, kAdjustLength(140)*3)];
+    _bottomView.backgroundColor = _contentView.backgroundColor;
+    [_contentView addSubview:_bottomView];
+    
+    UIView *forthLine = [[UIView alloc] initWithFrame:CGRectMake(10, 0, kScreen_Width-10, 1)];
+    forthLine.backgroundColor = [UIColor lightGrayColor];
+    [_bottomView addSubview:forthLine];
+    
+    //总计
+    _totalLab = [[UILabel alloc] initWithFrame:CGRectMake(10, 0, kScreen_Width-20, kAdjustLength(140))];
+    _totalLab.font = kFont_16;
+    _totalLab.textAlignment = NSTextAlignmentRight;
+    _totalLab.textColor = [UIColor redColor];
+    [_bottomView addSubview:_totalLab];
+    if (self.productModel.goods_dazhe.intValue == 1){
+         _totalLab.text  = [NSString stringWithFormat:@"总计:￥%@",self.productModel.goods_hprice];
+    }else{
+         _totalLab.text  = [NSString stringWithFormat:@"总计:￥%@",self.productModel.goods_price];
+    }
+    
+    UIView *fifthLine = [[UIView alloc] initWithFrame:CGRectMake(10, _totalLab.maxY, kScreen_Width-10, 1)];
+    fifthLine.backgroundColor = [UIColor lightGrayColor];
+    [_bottomView addSubview:fifthLine];
+
+    UIButton *addCarBtn = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    addCarBtn.frame = CGRectMake((kScreen_Width-kAdjustLength(500))/2.f, fifthLine.maxY+kAdjustLength(10), kAdjustLength(500), kAdjustLength(120));
+    addCarBtn.backgroundColor = [UIColor orangeColor];
+    [addCarBtn setTitle:@"加入购物车" forState:UIControlStateNormal];
+    [addCarBtn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    [addCarBtn addTarget:self action:@selector(addToCar:) forControlEvents:UIControlEventTouchUpInside];
+    addCarBtn.titleLabel.font = kFontSuper;
+    [_bottomView addSubview:addCarBtn];
+    
+    UIView *sixthLine = [[UIView alloc] initWithFrame:CGRectMake(10, addCarBtn.maxY+kAdjustLength(10), kScreen_Width-10, 1)];
+    sixthLine.backgroundColor = [UIColor lightGrayColor];
+    [_bottomView addSubview:sixthLine];
+
+    _contentView.contentSize = CGSizeMake(0, _bottomView.maxY);
+
 }
 
 #pragma mark - Click Menu
@@ -165,28 +219,8 @@
     }
 }
 
-#pragma mark - private _msgTextView
+- (void)addToCar:(UIButton *)btn{
 
-- (CGFloat)_getTextViewContentH:(UITextView *)textView
-{
-    if (IOSVersion >= 7.0)
-    {
-        return ceilf([textView sizeThatFits:textView.frame.size].height);
-    } else {
-        return textView.contentSize.height;
-    }
-}
-
-- (void)_willShowInputTextViewToHeight:(CGFloat)toHeight
-{
-    if (toHeight == _previousTextViewContentHeight)
-    {
-        return;
-    }
-    else{
-        CGFloat changeHeight = toHeight - _previousTextViewContentHeight;
-        
-    }
 }
 
 #pragma mark - UITextViewDelegate
@@ -199,7 +233,7 @@
 - (BOOL)textView:(UITextView *)textView shouldChangeTextInRange:(NSRange)range replacementText:(NSString *)text
 {
     if ([text isEqualToString:@"\n"]) {
-        [self _willShowInputTextViewToHeight:[self _getTextViewContentH:_msgTextView]];;
+        [textView resignFirstResponder];
         return NO;
     }
     return YES;
@@ -207,21 +241,29 @@
 
 - (void)textViewDidChange:(UITextView *)textView
 {
-//    [self _willShowInputTextViewToHeight:[self _getTextViewContentH:textView]];
     CGRect rect = textView.frame;
     rect.size.height = textView.contentSize.height;
     textView.frame = rect;
     [textView scrollRangeToVisible:NSMakeRange(0,0)];
+    _bottomView.frame = CGRectMake(0, _msgTextView.maxY+kAdjustLength(25), kScreen_Width, kAdjustLength(140)*3);
+    _contentView.contentSize = CGSizeMake(0, _bottomView.maxY);
+    _contentView.minY = self.iosChangeFloat+kNavHeight-_keyBoardHeight+kAdjustLength(140)*2-_msgTextView.height;
 }
 
 #pragma mark - keyboard
 - (void)keybordWillShow:(NSNotification *)noti{
+    NSDictionary *userInfo = [noti userInfo];
+    NSValue *aValue = [userInfo objectForKey:UIKeyboardFrameEndUserInfoKey];
+    CGRect keyboardRect = [aValue CGRectValue];
+    _keyBoardHeight = keyboardRect.size.height;
+    
     [UIView animateWithDuration:0.5 animations:^{
-        _contentView.minY = self.iosChangeFloat+kNavHeight-80;
+        _contentView.minY = self.iosChangeFloat+kNavHeight-_keyBoardHeight+kAdjustLength(140)*2-_msgTextView.height;
     }];
 }
 
 - (void)keybordWillHidden:(NSNotification *)noti{
+    _keyBoardHeight = 0;
     [UIView animateWithDuration:0.5 animations:^{
         _contentView.minY = self.iosChangeFloat+kNavHeight;
     }];
